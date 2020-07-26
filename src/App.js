932abf9b96f7dd2a3bd5e9fc61b0b457c3742f91
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-26 17:25:30 +0530

    Added Form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
-import MemberCard from "./components/MemberCard";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
-      <MemberCard />
     </div>
   );
 }
