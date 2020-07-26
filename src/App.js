cba0997269c01ba7b34287030a72359c79fb7c41
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-26 16:31:10 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import MemberCard from "./components/MemberCard";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Header />
+      <Form />
+      <MemberCard />
     </div>
   );
 }
