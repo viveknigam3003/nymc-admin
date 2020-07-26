28f2ae6e44a7e60aa8978b9822b0a508daa3ef78
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-26 19:27:29 +0530

    Updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,35 @@ import React from "react";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    root: {
-        width: "100%",
-        fontSize: 24,
-        fontFamily: "Work Sans",
-        fontWeight: 500,
-        paddingTop: 20,
-        paddingBottom: 20
-    }
+  root: {
+    width: "100%",
+    fontFamily: "Work Sans",
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 500,
+    padding: 10
+  },
+  sub: {
+    fontSize: 14,
+    color: "#000000",
+    opacity: 0.5,
+    fontWeight: 100
+  },
 });
 
 function Header() {
   const classes = useStyles();
-  return <div className={classes.root}>NYMC Membership Card </div>;
+  return (
+    <div className={classes.root}>
+      <div className={classes.title}>NYMC Membership Card</div>
+      <div className={classes.sub}>
+        Upload the image first and then fill in the candidate details
+      </div>
+    </div>
+  );
 }
 
 export default Header;
