7c69ff8257b75b6cad61e348910b5ea01acee3aa
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-26 19:22:05 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { grey, green } from "@material-ui/core/colors";
+
+const useStyles = makeStyles({
+  root: {
+    position: "relative",
+    left: 0,
+    bottom: 0,
+    backgroundColor: green["A200"],
+    color: "#000000",
+    width: "100%",
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    opacity: 0.8,
+    padding: 10,
+    fontSize: 10,
+    fontFamily: "Work Sans",
+  },
+});
+
+function Footer() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div className={classes.text}>Developed by Vivek Nigam (viveknigam3003) | 2020</div>
+    </div>
+  );
+}
+
+export default Footer;
